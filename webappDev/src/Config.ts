dcc28fc244c9faa4ee3b96d012429cfdc412bfc5
Noah Albers <albers-noah@pm.me>
2025-12-27 15:16:54 +0100

    Test 3 of new movement

--- webappDev/src/Config.ts
+++ webappDev/src/Config.ts
@@ -1,6 +1,6 @@
 
 // Which address to use for the websockets
-export const ADDRESS = 'localhost:5000'; // AUTO for automatic
+export const ADDRESS = 'AUTO'; // AUTO for automatic
 
 // Which emojis to activate
 export const EMOJIS: {[key: string]: string} = {
